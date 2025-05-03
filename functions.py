d65e331f4232314178dcbf2f94be8957b105834e
Bushra Fatima <bushrafatima@MacBook-Pro-2.local>
2025-05-03 19:38:24 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+# functions = a block of code which is executed only when it is called 
+
+def greeting(fname, lname):
+    print("Hello "+ fname + " " + lname)
+
+fname = "bro"
+lname = "code"
+
+greeting(fname, lname)
