c79983048cd2677018be9279345257da8c28d6b3
Bushra Fatima <bushrafatima@MacBook-Pro-2.local>
2025-05-03 16:19:17 +0100

    Bro Code - while loops

---
+++ while_loop.py
@@ -0,0 +1,8 @@
+# while loops = a statement that will execute it's block of code, as long as it's condition is true 
+
+#name = ""
+
+#while len(name) == 0:
+#    name = input("What is your name: ")
+
+#print("Hello " + name)
