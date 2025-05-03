52d6688efdd9c1a925de15d6f0df5e8f33ec1f0b
Bushra Fatima <bushrafatima@MacBook-Pro-2.local>
2025-05-03 11:56:58 +0100

    bro code - multiple assignment

---
+++ python/multiple_assignments.py
@@ -0,0 +1,25 @@
+# multiple assignments = allows us to assign multiple 
+# two diff ways to use multiple assignment: 
+    # if you have diff values assign variables in order like below 
+
+#name = "bro"
+#age = 21
+#race = "white"
+
+name, age, race = "bro", 21, "white"
+
+print(name)
+print(age)
+print(race)
+
+    # if you have the same values across variables - then use the = sign 
+
+#squidward = 30
+#spongebob = 30 
+#patrick = 30 
+
+squidward = spongebob = patrick = 30
+
+print(squidward)
+print(spongebob)
+print(patrick)
