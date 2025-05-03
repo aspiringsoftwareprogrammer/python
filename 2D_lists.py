7d3cfb22b0d5b10cbdb7dbaa7aabe74a15e1c41d
Bushra Fatima <bushrafatima@MacBook-Pro-2.local>
2025-05-03 18:33:31 +0100

    Bro Code - multidimensional lists

---
+++ 2D_lists.py
@@ -0,0 +1,10 @@
+# 2D lists (aka multidimensional lists) = a list of lists 
+# everything always starts at 0
+
+country = ["england"]
+cities = ["london", "manchester", "birmingham", "bristol"]
+county = ["hampshire", "cheshire", "cumbria", "northamptonshire"]
+
+england = [country, cities, county]
+
+print(england[1][3])
