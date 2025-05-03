c226efa12656aedbc910271ad2be95902aa73dcb
Bushra Fatima <bushrafatima@MacBook-Pro-2.local>
2025-05-03 19:40:37 +0100

    return statement

---
+++ return_statement.py
@@ -0,0 +1,6 @@
+# functions send python values/objects back to the caller, these values/objects are known as the function's return values 
+
+def multiply(num1, num2):
+    return num1 * num2
+
+print(multiply(6,9))
